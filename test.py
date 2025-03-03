e2ca427430f7627d445e9fc741e3a4c1bf8f078d
sampathkaran <50194345+sampathkaran@users.noreply.github.com>
2025-03-03 18:52:38 +0800

    prtest1

---
+++ test.py
@@ -0,0 +1,9 @@
+# test.py
+
+def test_function():
+    # This function will fail the test if the condition is not met
+    assert 1 + 1 == 2  # Change this condition to make it fail if needed
+
+if __name__ == "__main__":
+    test_function()
+    print("Test Passed!")
